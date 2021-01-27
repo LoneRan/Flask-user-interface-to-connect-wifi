d745c9c05b332df6ad14d33e76ef5e3d16072def
Yibo Wang <yibow@usc.edu>
2021-01-27 11:48:18 -0800

    add virtual keyboard

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import requests
 import json
 app = Flask(__name__)
-
+CHECK = 0
 @app.route("/")
 def home():
-    return render_template("wifi.html")
+    if(CHECK == 0):
+        return render_template("wifi.html")
+    URL = "http://localhost:3000/check"
+    try:
+        uResponse = requests.get(URL)
+    except requests.ConnectionError:
+       return "Connection Error"
+
+    response = json.loads(uResponse.text)
+    if(response['status'] == 'Yes'):
+        return redirect("http://finalmodel.azurewebsites.net", code=302)
+    else:
+        return render_template("wifi.html")
 
 
 @app.route('/form')
@@ -23,7 +35,7 @@ def form():
     print(choices)
     return render_template('form.html',choices=choices)
 
-@app.route('/data/', methods = ['POST', 'GET'])
+@app.route('/data/', methods = ['POST','GET'])
 def data():
     if request.method == 'GET':
         return f"The URL /data is accessed directly. Try going to '/form' to submit form"
